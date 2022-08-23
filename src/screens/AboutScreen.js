87180caff507192aa98b0e09d6ada5d2884011e6
Adams-Ganiwu <ibrahimabdulatif1@gmail.com>
2022-08-23 17:25:38 +0000

    routing

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../components/Header';
 
 function AboutScreen() {
   return (
-    <div>AboutScreen</div>
+    <div>
+      <Header />
+      <p>This webpag contains countries and there names there currencies,captals,flags and there languages </p>
+    
+    </div>
   )
 }
 
